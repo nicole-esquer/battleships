15af7307d66e8c91c46aa9c1afb8bb5b11e1fde3
Blake Saylor <blakesaylor@gmail.com>
2022-05-17 09:44:07 -0600

    Create empty? method to check if ship is present

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,16 @@
 class Cell
   attr_reader :coordinate, :ship
+
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+  end
+
+  def empty?
+    if @ship == nil
+      return true
+    end
+    return false
   end
 
 end
