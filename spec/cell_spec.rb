9415dfcf349f1039d86a8ee3ac7663e6255de486
Nicole Esquer <nicole.esquer5@gmail.com>
2022-05-17 13:14:01 -0700

    Test: ship returns nil

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,9 +1,11 @@
 require './lib/cell'
-require './lib/cell'
+require './lib/ship'
 
 RSpec.describe Cell do
   before :each do
     @cell = Cell.new("B4")
+    @ship = Ship.new("cruiser", 3)
+
   end
 
   it "exists" do
@@ -14,7 +16,9 @@ RSpec.describe Cell do
     expect(@cell.coordinate).to eq("B4")
   end
 
-
+  it "returns nil when ship is called" do
+    expect(@cell.ship).to eq(nil)
+  end
 
 
 end
