db8717362d1164750ae1c40de54e203cc444f5e8
Blake Saylor <blakesaylor@gmail.com>
2022-05-21 14:10:36 -0600

    Add initialization, valid_coordinate? method to pass tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,22 @@
 class Board
-  attr_reader :cells
-  
+  attr_reader :cells, :rows, :columns
+
   def initialize
+    @rows = []
+    @columns = []
     @cells = {}
+
+    ("A".."D").each do |letter|
+      # @rows << letter
+      (1..4).each do |number|
+        # @columns << number.to_s
+        @cells["#{letter}#{number.to_s}"] = Cell.new("#{letter}#{number.to_s}")
+      end
+    end
+  end
+
+  def valid_coordinate?(coordinate)
+    @cells.key?(coordinate)
   end
 
 end
