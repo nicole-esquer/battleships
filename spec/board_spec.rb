9b8f72f771c986f7671f6003d7c9a769ea3c8791
Blake Saylor <blakesaylor@gmail.com>
2022-05-21 14:22:52 -0600

    Unskipping tests until failure

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,12 +24,12 @@ RSpec.describe Board do
     expect(@board.valid_coordinate?("A22")).to eq(false)
   end
 
-  xit "has coords that equal length of ship" do
+  it "has coords that equal length of ship" do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to eq(false)
       expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to eq(false)
   end
 
-  xit "has coords that are consecutive" do
+  it "has coords that are consecutive" do
     expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
     expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
     expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
