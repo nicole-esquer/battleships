bbc17ee8a438833d80b9a2cea448ea41476c47de
Blake Saylor <blakesaylor@gmail.com>
2022-05-21 15:16:32 -0600

    Add valid_placement? and helpers to pass tests

--- lib/board.rb
+++ lib/board.rb
@@ -19,7 +19,6 @@ class Board
     @cells.key?(coordinate)
   end
 
-
   def valid_placement?(ship, coordinates)
     coordinates.each do |coordinate|
       if valid_coordinate?(coordinate) == false
@@ -30,21 +29,45 @@ class Board
     if (ship.length != coordinates.length)
       return false
     end
+
+    # Make sure it is x consecutive cells in horizontal or vertical order
+    is_horizontal?(coordinates) || is_vertical?(coordinates)
+  end
+
+  def is_horizontal?(coordinates)
+    letters = coordinates.map{ |coordinate| coordinate[0].ord }
+    numbers = coordinates.map { |coordinate| coordinate[1].ord }
+
+    same_letter = false
+    if letters.uniq.count == 1
+      same_letter = true
+    end
+
+    # Need to figure out how to find if consecutive letters/numbers
+    consecutive_numbers = false
+    if numbers.each_cons(2).all? {|i,j| j == i + 1}
+      consecutive_numbers = true
+    end
+
+    same_letter && consecutive_numbers
+  end
+
+  def is_vertical?(coordinates)
+    letters = coordinates.map{ |coordinate| coordinate[0].ord }
+    numbers = coordinates.map { |coordinate| coordinate[1].ord }
+
+    same_number = false
+    if numbers.uniq.count == 1
+      same_number = true
+    end
+
+    # Need to figure out how to find if consecutive letters/numbers
+    consecutive_letters = false
+    if letters.each_cons(2).all? {|i,j| j == i + 1}
+      consecutive_letters = true
+    end
+
+    same_number && consecutive_letters
   end
 
 end
-
-
-
-
-
-
-# numbers = [1, 2, 3, 4]
-# letters = ["A", "B", "C", "D"]
-#
-# cells = {}
-# letters.each do |letter|
-#   numbers.each do |number|
-#     cells_hash["#{letter}#{number.to_s}"] = Cell.new("#{letter}#{number.to_s}")
-# end
-# end
