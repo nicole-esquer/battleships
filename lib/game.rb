b0b3126d33b69f6b03c0309a04f32530f6f3c8cb
Blake Saylor <blakesaylor@gmail.com>
2022-05-23 17:15:03 -0600

    Begin iterating through game procession

--- lib/game.rb
+++ lib/game.rb
@@ -3,13 +3,12 @@ require './lib/cell'
 require './lib/ship'
 require './lib/computer'
 
-
-
 class Game
+  attr_reader :player_board, :computer_board
 
   def initialize
-    @cruiser = Ship.new('Cruiser', 3)
-    @submarine = Ship.new('Submarine', 2)
+    @player_board = Board.new
+    @computer_board = Board.new
   end
 
   def main_menu
@@ -19,23 +18,50 @@ class Game
 
     if response == "p"
       puts "Let's go!"
-      #start
-    elsif
+      gamestate
+    elsif response == "q"
       puts "Bye!"
-      #exit game, return to main_menu
-    else response != "p" || "q"
+    else
       puts "I'm sorry, that's an invalid entry. Please try again."
-      #return to main_menu
+      main_menu
     end
   end
+
+  def place_computer_ships
+    computer_cruiser = Ship.new('Cruiser', 3)
+    computer_submarine = Ship.new('Submarine', 2)
+    @computer_board.place(computer_cruiser, @computer_board.get_valid_coordinates(computer_cruiser))
+        @computer_board.place(computer_submarine, @computer_board.get_valid_coordinates(computer_submarine))
+  end
+
 
   def start
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
-    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts "The cruiser is three units long and the submarine is two units long."
+    player_board_render = @player_board.render(true)
+    puts player_board_render
+  end
+
+  def player_place_cruiser
+    puts "Enter the coordinates to use for your cruiser (3 coordinates):"
+    puts "Type them in this format: A1 A2 A3"
+    player_cruiser_coords = gets.chomp.upcase.split
+    if @player_board.valid_placement?(player_cruiser, player_cruiser_coords)
+      @player_board.place(player_cruiser, player_cruiser_coords)
+      @player_board.render(true)
+      player_place_submarine
+    else
+      puts "Those coordinates are not valid. Please try again."
+      player_place_submarine
+    end
+  end
+
+  def player_place_submarine
 
   end
 
-
-
 end
+
+# game = Game.new
+# game.main_menu
