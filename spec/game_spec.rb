94d006e3f2cbfa5524e030c5f036fd7ad81aed77
Nicole Esquer <nicole.esquer5@gmail.com>
2022-05-24 14:10:11 -0700

    Initializing game attributes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,18 +10,9 @@ RSpec.describe Game do
     @computer_ships_count = 2
   end
 
-  it 'exists' do
-    expect(@game).to be_instance_of(Game)
-    expect(@player_board).to be_instance_of(Board)
-    expect(@computer_board).to be_instance_of(Board)
-  end
 
-  it "attributes" do
-    computer_cruiser = Ship.new('Cruiser', 3)
-    computer_submarine = Ship.new('Submarine', 2)
 
-    expect(computer_cruiser).to be_instance_of(Ship)
-    expect(computer_submarine).to be_instance_of(Ship)
-  end
-  
+
+
+
 end
