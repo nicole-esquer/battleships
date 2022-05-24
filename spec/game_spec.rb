3e74710cac35081c445c01762f0ab0bd7871f7cf
Nicole Esquer <nicole.esquer5@gmail.com>
2022-05-24 13:09:19 -0700

    Created tests for attributes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,28 @@
 require './lib/game'
 
 RSpec.describe Game do
-  it 'exists' do
+  before (:each) do
     @game = Game.new
+    @board = Board.new
+    @player_board = Board.new
+    @computer_board = Board.new
+    @player_ships_count = 2
+    @computer_ships_count = 2
+  end
+
+  it 'exists' do
     expect(@game).to be_instance_of(Game)
-end
+    expect(@player_board).to be_instance_of(Board)
+    expect(@computer_board).to be_instance_of(Board)
+  end
+
+  it "attributes" do
+    computer_cruiser = Ship.new('Cruiser', 3)
+    computer_submarine = Ship.new('Submarine', 2)
+
+    expect(computer_cruiser).to be_instance_of(Ship)
+    expect(computer_submarine).to be_instance_of(Ship)
+  end
+
 
 end
